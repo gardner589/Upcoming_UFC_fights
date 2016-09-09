a3499a7b648d83bbc5dde9d43c82aace0667d267
Chris Gardner <ctgardner89@gmail.com>
2016-09-09 15:17:40 -0400

    init commit

---
+++ skrip.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $.getJSON('http://ufc-data-api.ufc.com/api/v1/us/events').then(function(res){
+    $.each(res,function(i,item){
+      $('.test').append('<div><div class="tester"><img src="'+item.feature_image+'"/><h2>'+item.title_tag_line+'</h2><h3>'+item.base_title+'</h3><p>'+new Date(item.event_date)+'</p></div></div>')
+    })
+    $(".test").slick({
+      dots: true,
+      speed: 500,
+      infinite: true
+    })
+  })
+})
