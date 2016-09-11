7a10ce1d89d447ce01563a90b10bff4df0fa407d
Chris Gardner <ctgardner89@gmail.com>
2016-09-10 21:13:36 -0400

    seeing if this works

--- skrip.js
+++ skrip.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
+  $.ajaxSetup({
+    headers: {
+      'Origin' : '*'
+    }
+  });
   $.getJSON('http://ufc-data-api.ufc.com/api/v1/us/events').then(function(res){
     $.each(res,function(i,item){
+      // console.log(item)
       $('.test').append('<div><div class="tester"><img src="'+item.feature_image+'"/><h2>'+item.title_tag_line+'</h2><h3>'+item.base_title+'</h3><p>'+new Date(item.event_date)+'</p></div></div>')
     })
     $(".test").slick({
